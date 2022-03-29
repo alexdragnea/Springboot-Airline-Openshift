7f6a245493e4aa16c3e67e13bcaedf7be5a7076e
Alexandru Dragnea <alexdg722@gmail.com>
2022-03-29 16:38:01 +0300

    Development increment

--- src/main/java/net/dg/springbootrestapi/model/PassengerResponse.java
+++ src/main/java/net/dg/springbootrestapi/model/PassengerResponse.java
@@ -1,8 +1,10 @@
 package net.dg.springbootrestapi.model;
 
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@SuperBuilder
 public class PassengerResponse {
 
     private String _id;
